3cd0fe047fc8d2dd034e976ff8b9d1c889dc7e7e
Wency Salta <wencysalta.ws@gmail.com>
2024-03-18 13:52:13 +0800

    Link Image Error

---
+++ app/news/error.js
@@ -0,0 +1,11 @@
+"use client";
+import Error from "next/error";
+export default function GlobalError({ error }) {
+  return (
+    <html>
+      <body>
+        <Error statusCode={500}  />
+      </body>
+    </html>
+  );
+}
---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  images: {
+    domains: ["blogger.googleusercontent.com"],
+  },
+};
