cac0b63347419e99f56dbf8b494eeece9e7b0175
Paolo Lalap <paolomorales1502@gmail.com>
2024-04-08 09:03:04 +0800

    Change the extension to jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,5 @@
 import { Inter, Open_Sans } from "next/font/google";
-import "./reset.css";
+import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
