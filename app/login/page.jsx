9413f96fec2a5d76014d1e5fb2cc5667ed496ca6
Paolo Lalap <paolomorales1502@gmail.com>
2024-04-11 09:19:05 +0800

    Add login

---
+++ app/login/page.jsx
@@ -0,0 +1,18 @@
+import LoginForm from "@/components/LoginForm";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+
+export default async function LoginPage() {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+     redirect("/");     
+   }
+
+  return (
+    <main>  
+      <LoginForm />
+    </main>
+  );
+}
