5dc4c903b1a54d985f273d1951323456e3d3c9cc
Wency Salta <wencysalta.ws@gmail.com>
2024-04-11 09:45:42 +0800

    rename Home to ToolsPage

--- app/tools/page.jsx
+++ app/tools/page.jsx
@@ -1,6 +1,6 @@
 
 import Urlchecker from "@/components/Urlchecker";
-export default function Home() {
+export default function ToolsPage() {
   return (
     <div className="h-max  items-center bg-[#f7f7e3]">
       
