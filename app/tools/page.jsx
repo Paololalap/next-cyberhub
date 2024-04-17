e0756bae3ea1d2365176463e9388af24a785ef7a
Paolo Lalap <paolomorales1502@gmail.com>
2024-04-17 15:42:28 +0800

    fix merge conflicts

--- app/tools/page.jsx
+++
@@ -1,9 +0,0 @@
-import PasswordChecker from "@/components/PasswordChecker";
-export default function ToolsPage() {
-  return (
-    <div className="h-max items-center bg-[#f7f7e3]">
-          <PasswordChecker />
-          
-    </div>
-  );
-}
