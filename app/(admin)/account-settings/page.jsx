848f9611fdcbc8246444ba8c26725a29d594a263
Paolo Lalap <paolomorales1502@gmail.com>
2024-04-30 09:51:30 +0800

    add account-settings page

--- app/(admin)/account-settings/page.jsx
+++ app/(admin)/account-settings/page.jsx
@@ -1,19 +1,9 @@
-// app/account-settings/page.jsx
-"use client";
-import AccountEditForm from "@/components/AccountEditForm";
-import { useSession } from "next-auth/react";
-
-export default function AccountSettingsPage() {
-  const { data: session } = useSession();
-  console.log(session?.user);
+import EditUserForm from "@/components/AccountEditParent";
+export default async function AccountSettingsPage() {
+ 
   return (
     <>
-      <AccountEditForm
-        firstName={firstName}
-        lastName={lastName}
-        username={username}
-        profilePic={profilePic}
-      />
+      <EditUserForm/>
     </>
   );
 }
