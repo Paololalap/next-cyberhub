2dc0a5eb5501a5fba0b2824c0888f1435e681f68
Paolo Lalap <paolomorales1502@gmail.com>
2024-04-30 09:49:40 +0800

    update nextauth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -28,14 +28,31 @@ export const authOptions = {
             return null;
           }
 
-          const userinfo = { _id: user._id };
-          return {...user, ...userinfo}
+          return {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            // isAdmin: user.isAdmin,
+          };
         } catch (error) {
           console.log("Error: ", error);
         }
       },
     }),
   ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user?._id) token._id = user._id;
+      if (user?.isAdmin) token.isAdmin = user.isAdmin;
+      return token;
+    },
+    async session({ session, token }) {
+      if (token?._id) session.user._id = token._id;
+      if (token?.isAdmin) session.user.isAdmin = token.isAdmin;
+      return session;
+    },
+  },
+
   session: {
     strategy: "jwt",
   },
