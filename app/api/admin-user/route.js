8ebea4fe4d1e6c4ed562ab52eebec4641cf6e08f
Paolo Lalap <paolomorales1502@gmail.com>
2024-04-30 09:49:05 +0800

    admin-user api

--- app/api/admin-user/route.js
+++ app/api/admin-user/route.js
@@ -2,7 +2,7 @@
 
 import connectMongoDB from "@/lib/db";
 import User from "@/models/user";
-import { getServerSession,  } from "next-auth/next";
+import { getServerSession } from "next-auth/next";
 import { authOptions } from "../auth/[...nextauth]/route";
 import { NextResponse } from "next/server";
 
@@ -18,10 +18,41 @@ export async function GET(request) {
   }
 
   // Access the user data from the session object
-  const userId = session.id;
-  console.log(session);
-  // Use the userId to fetch user data or perform other operations
-  const user = await User.find();
+  const userId = session.user._id;
 
+  // Use the userId to fetch user data
+  const user = await User.findById({ _id: userId });
+
+  if (!user) {
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  }
+
+  // const { firstName, lastName, username, profilePic } = user;
   return NextResponse.json({ user });
+}
+
+export async function PUT(request) {
+  const session = await getServerSession(authOptions);
+
+  // Check if the user is authenticated
+  if (!session) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  // Access the user data from the session object
+  const userId = session.user._id;
+  const {
+    newFirstName: firstName,
+    newLastName: lastName,
+    newUsername: username,
+    newProfilePic: profilePic,
+  } = await request.json();
+  await connectMongoDB();
+  await User.findByIdAndUpdate(userId, {
+    firstName,
+    lastName,
+    username,
+    profilePic,
+  });
+  return NextResponse.json({ message: "User updated" }, { status: 200 });
 }
