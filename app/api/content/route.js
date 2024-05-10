c40d65721b978a8125ae4bd4925014713a92130e
Paolo Lalap <paolomorales1502@gmail.com>
2024-05-10 09:41:33 +0800

    add headline in homepage

--- app/api/content/route.js
+++ app/api/content/route.js
@@ -2,8 +2,6 @@ import connectMongoDB from "@/lib/db";
 import Content from "@/models/content";
 import { NextResponse } from "next/server";
 
-
-
 export async function POST(request) {
   const { title, tags, author, date, link, description, body, imageL, type } =
     await request.json({ limit: "2mb" });
@@ -29,4 +27,12 @@ export async function DELETE(request) {
   await connectMongoDB();
   await Content.findByIdAndDelete(id);
   return NextResponse.json({ message: "Content deleted" }, { status: 200 });
+}
+
+export async function GET() {
+  await connectMongoDB();
+  const content = await Content.find({ type: "News" })
+    .sort({ createdAt: -1 })
+    .limit(1);
+  return NextResponse.json({ content });
 }
--- app/layout.jsx
+++ app/layout.jsx
@@ -15,10 +15,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* <Header /> */}
+        <Header />
         {children}
         <Toaster />
-        {/* <Footer className={openSans.className} /> */}
+        <Footer className={openSans.className} />
       </body>
     </html>
   );
