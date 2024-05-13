8b7dffdbf629d59120ff3b0e1d10a85f5556c473
Wency Salta <wencysalta.ws@gmail.com>
2024-05-13 10:05:19 +0800

    add pinStatus

--- models/thread.js
+++ models/thread.js
@@ -15,10 +15,11 @@ const commentSchema = new Schema(
     author: String,
     content: String,
     postId: { type: Schema.Types.String, ref: "Post" },
+    pinStatus: String,
   },
   {
     timestamps: true,
-  }
+  },
 );
 const Post = mongoose.models.Post || mongoose.model("Post", postSchema);
 const Comment =
