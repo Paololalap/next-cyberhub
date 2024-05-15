4e05319cf5e5ebfc90c7c1c5d5d0187ec26216c3
Wency Salta <wencysalta.ws@gmail.com>
2024-05-15 16:30:51 +0800

    working delete

--- app/api/announces/[id]/route.js
+++ app/api/announces/[id]/route.js
@@ -6,7 +6,7 @@ import { NextResponse } from "next/server";
 // GET endpoint to fetch a specific announcement by ID
 export async function GET() {
   await connectMongoDB();
-  const announces = await Announces.find();
+  const announces = await Content.findOne({ _id: id });
   return NextResponse.json({ announces });
 }
 
