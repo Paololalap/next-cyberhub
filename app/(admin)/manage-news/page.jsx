b2984b3805e53b3b231d9db7b731902c30ecbe70
Paolo Lalap <paolomorales1502@gmail.com>
2024-05-17 14:13:41 +0800

    update manage-news

--- app/(admin)/manage-news/page.jsx
+++ app/(admin)/manage-news/page.jsx
@@ -5,7 +5,7 @@ import AddEntry from "@/components/button/AddEntry";
 
 export default function ManageNewsPage({ searchParams }) {
   return (
-    <div className="flex min-h-screen w-screen flex-col bg-[#f7f7e3] px-3">
+    <div className="flex min-h-screen w-screen flex-col bg-[#f7f7e3]">
       <div className="mt-5 text-center text-3xl font-black">Manage News</div>
       <hr className="mx-auto mt-3 w-64 border-2 border-solid border-[#FFB61B]" />
       <AddEntry>Add News Entry</AddEntry>
