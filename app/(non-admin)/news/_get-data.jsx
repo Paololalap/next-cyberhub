a6d5956c1487d492cd40390288d8dcfa03fbdc01
Paolo Lalap <paolomorales1502@gmail.com>
2024-05-22 11:43:24 +0800

    update ui

--- app/(non-admin)/news/_get-data.jsx
+++ app/(non-admin)/news/_get-data.jsx
@@ -74,7 +74,7 @@ export default async function GetData({ searchParams }) {
         {pageNumber === 1 && data.latestNews && (
           <div
             key={data.latestNews._id}
-            className="group mt-2 grid grid-cols-1 rounded-md border-2 border-solid border-[#00563F] bg-white first:mt-0 md:grid-cols-12"
+            className="group mt-2 grid grid-cols-1 rounded-md border-2 border-solid border-[#00563F] bg-white first:mt-0 md:grid-cols-12 overflow-hidden"
           >
             <Link
               href={`/article/${data.latestNews._id}`}
@@ -116,7 +116,7 @@ export default async function GetData({ searchParams }) {
         {data.items.map((item) => (
           <div
             key={item._id}
-            className="group mt-2 grid grid-cols-1 rounded-md border-2 border-solid border-[#00563F] bg-white first:mt-0 md:grid-cols-12"
+            className="group mt-2 grid grid-cols-1 rounded-md border-2 border-solid border-[#00563F] bg-white first:mt-0 md:grid-cols-12 overflow-hidden"
           >
             <Link
               href={`/article/${item._id}`}
