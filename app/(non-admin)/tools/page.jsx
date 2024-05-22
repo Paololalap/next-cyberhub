ca052f0b2e6061af73662566fdce0df0cf113ff0
Paolo Lalap <paolomorales1502@gmail.com>
2024-05-22 13:49:22 +0800

    add loading spinner

--- app/(non-admin)/tools/page.jsx
+++ app/(non-admin)/tools/page.jsx
@@ -1,10 +1,18 @@
 import PasswordChecker from "@/components/PasswordChecker";
 import UrlChecker from "@/components/UrlChecker";
+import { Suspense } from "react";
+import { Loader2 } from "lucide-react";
+
 export default function ToolsPage() {
   return (
-    <div className="min-h-max w-fit items-center py-5">
-      <PasswordChecker />
-      <UrlChecker />
+    <div className="min-h-max w-screen grid place-items-center py-5">
+      <Suspense fallback={<Loader2 className="mx-auto size-14 animate-spin" />}>
+        <PasswordChecker />
+      </Suspense>
+
+      <Suspense fallback={<Loader2 className="mx-auto size-14 mt-20 animate-spin" />}>
+        <UrlChecker />
+      </Suspense>
     </div>
   );
 }
