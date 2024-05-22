9b0ed6ec5011316119cb9917f9aeb7e12ebdf936
Paolo Lalap <paolomorales1502@gmail.com>
2024-05-22 11:23:23 +0800

    update import

--- app/(admin)/manage-tips/page.jsx
+++ app/(admin)/manage-tips/page.jsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from "react";
 import AddEntry from "@/components/button/AddEntry";
 import GetData from "./_get-data";
-import LoadingSkeleton from "@/components/LoadingSkeleton";
+import LoadingSkeleton from "@/components/loading/LoadingSkeleton";
 
 export default function TipsPage({ searchParams }) {
   return (
