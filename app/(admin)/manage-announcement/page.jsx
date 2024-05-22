022f571cffd02c4ed61a1c4ef1c7b49be04b33c5
Paolo Lalap <paolomorales1502@gmail.com>
2024-05-22 11:14:48 +0800

    update manage-announcement page

--- app/(admin)/manage-announcement/page.jsx
+++ app/(admin)/manage-announcement/page.jsx
@@ -1,148 +1,16 @@
-import React from "react";
-import { connectToDatabase } from "@/lib/connectMongo";
-import Link from "next/link";
-import RemoveAnn from "@/components/RemoveAnn";
-import formatDateToWords from "@/constants/DATE_TO_WORDS";
-
-async function getData(perPage, pageNumber) {
-  try {
-    // DB Connect
-    const client = await connectToDatabase();
-    const db = client.db("CyberDB");
-
-    // DB Query
-    const items = await db
-      .collection("announces")
-      .find({})
-      .sort({ startDate: -1 })
-      .skip(perPage * (pageNumber - 1))
-      .limit(perPage)
-      .toArray();
-
-    const itemCount = await db.collection("announces").countDocuments({});
-
-    const response = { items, itemCount };
-    return response;
-  } catch (error) {
-    throw new Error("Failed to fetch data. Please try again later.");
-  }
-}
+import GetData from "./_get-data";
+import AddAnnouncementButton from "@/components/button/AddAnnouncement";
 
 export default async function AnnouncementsPage({ searchParams }) {
-  let page = parseInt(searchParams.page, 10);
-  page = !page || page < 1 ? 1 : page;
-  const perPage = 8;
-  const data = await getData(perPage, page);
-
-  const totalPages = Math.ceil(data.itemCount / perPage);
-
-  const prevPage = page - 1 > 0 ? page - 1 : 1;
-  const nextPage = page + 1;
-  const isPageOutOfRange = page > totalPages;
-
-  const pageNumbers = [];
-  const offsetNumber = 3;
-  for (let i = page - offsetNumber; i <= page + offsetNumber; i++) {
-    if (i >= 1 && i <= totalPages) {
-      pageNumbers.push(i);
-    }
-  }
-
   return (
-    <div className="h-max items-center bg-[#f7f7e3]">
-      <div className="flex-col items-center justify-center bg-transparent">
-        <div className="flex-col items-center justify-center p-5 text-center text-2xl font-semibold text-[#6e102c]">
-          <span>Manage Announcements</span>
-          <hr className="mx-auto w-64 border-2  border-solid border-[#FFB61B]" />
-        </div>
+    <div className="flex w-screen flex-col bg-[#f7f7e3]">
+      <div className="mt-5 text-center text-3xl font-black">
+        Manage Announcements
       </div>
-      <div className="flex flex-col items-center justify-center">
-        <Link
-          href={"/add-announcement"}
-          className="rounded-md border-2 border-solid border-[#00563F] p-2"
-        >
-          Add Announcement
-        </Link>
-      </div>
-      <div className="my-5 flex flex-col items-center justify-center sm:mx-80">
-        {data.items.map((item) => (
-          <div key={item._id} className="mb-1">
-            <div className="group flex max-h-56 flex-row overflow-hidden rounded-md border-2 border-solid border-[#00563F] bg-white sm:flex sm:max-h-56 sm:flex-row">
-              <div className="flex w-3/5 flex-col justify-between p-5 sm:flex sm:flex-col sm:p-5">
-                <div className="sm:flex sm:flex-col">
-                  <div className="text-sm font-bold text-gray-500 group-hover:underline sm:text-sm">
-                    {item.title}
-                  </div>
-                  <div className="mb-5">
-                    <div className="mb-1 mr-10 text-xs sm:text-sm">
-                      <span>
-                        {formatDateToWords(item.startDate)} to{" "}
-                        {formatDateToWords(item.endDate)}
-                      </span>
-                    </div>
-                    {/* Tags can be displayed here */}
-                  </div>
-                  <div className="text-xs sm:text-sm">
-                    <span>{item.content}</span>
-                  </div>
-                </div>
-                <div className="flex flex-row-reverse text-xs sm:text-sm">
-                  <Link
-                    href={`/edit-announcement/${item._id}`}
-                    className="mx-1 rounded-md border-2 border-solid border-[#00563F] p-2"
-                  >
-                    Update
-                  </Link>
-
-                  <RemoveAnn id={item._id.buffer.toString("hex")} />
-                </div>
-              </div>
-            </div>
-          </div>
-        ))}
-
-        {isPageOutOfRange ? (
-          <div>No more pages...</div>
-        ) : (
-          <div className="mt-16 flex items-center justify-center">
-            <div className="border-light-green flex gap-4 rounded-[10px] border-[1px] p-4">
-              {page === 1 ? (
-                <div className="opacity-60" aria-disabled="true">
-                  Previous
-                </div>
-              ) : (
-                <Link href={`?page=${prevPage}`} aria-label="Previous Page">
-                  Previous
-                </Link>
-              )}
-
-              {pageNumbers.map((pageNumber, index) => (
-                <Link
-                  key={index}
-                  className={
-                    page === pageNumber
-                      ? "rounded-md bg-[#00563f] px-2 text-[#FFB61B]"
-                      : "rounded-md px-1 hover:bg-[#00563f]"
-                  }
-                  href={`?page=${pageNumber}`}
-                >
-                  {pageNumber}
-                </Link>
-              ))}
-
-              {page === totalPages ? (
-                <div className="opacity-60" aria-disabled="true">
-                  Next
-                </div>
-              ) : (
-                <Link href={`?page=${nextPage}`} aria-label="Next Page">
-                  Next
-                </Link>
-              )}
-            </div>
-          </div>
-        )}
-      </div>
+      <hr className="mx-auto mt-3 w-64 border-2 border-solid border-[#FFB61B]" />
+      <AddAnnouncementButton />
+      
+      <GetData searchParams={searchParams} />
     </div>
   );
 }
