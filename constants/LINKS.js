e7426d44998334973fe0a9a6707db268f8eacd84
Paolo Lalap <paolomorales1502@gmail.com>
2024-05-22 11:45:55 +0800

    update

--- constants/LINKS.js
+++ constants/LINKS.js
@@ -1,6 +1,6 @@
 export const LINKS = [
   { id: "news", href: "/news", text: "News" },
-  { id: "tips", href: "/tips", text: "Tips & Guides" },
+  { id: "tips", href: "/tips", text: "Tips" },
   { id: "tools", href: "/tools", text: "Tools" },
   { id: "community", href: "/community", text: "Community" },
   { id: "about", href: "/about", text: "About" },
