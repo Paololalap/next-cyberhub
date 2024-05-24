031977a24128f0292ad6ff4e7318ddb0e532734d
Paolo Lalap <paolomorales1502@gmail.com>
2024-05-24 13:46:14 +0800

    remove loader

--- components/button/ReadMore.jsx
+++ components/button/ReadMore.jsx
@@ -1,18 +1,12 @@
-"use client";
 import Link from "next/link";
-import { Button } from "../ui/button";
-import { useState } from "react";
-import { Loader2 } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 export default function ReadMore({ id }) {
-  const [isLoading, setIsLoading] = useState(false);
   return (
     <Link href={`/article/${id}`} tabIndex={-1}>
       <Button
         className="bg-[#8a1438] hover:bg-[#8a1438]/90"
-        onClick={() => setIsLoading((prev) => !prev)}
       >
-        {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
         Read more
       </Button>
     </Link>
