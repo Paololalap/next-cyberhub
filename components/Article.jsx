4647b13c88e19a6d8c21752f8fabe2bbc2290737
Paolo Lalap <paolomorales1502@gmail.com>
2024-05-26 08:42:14 +0800

    remove required image

--- components/Article.jsx
+++ components/Article.jsx
@@ -19,7 +19,7 @@ export default function Article({
   link,
 }) {
   return (
-    <div className="flex flex-col bg-white p-8 sm:min-h-screen lg:mx-auto lg:w-[75rem]">
+    <div className="flex flex-col bg-white p-8 lg:mx-auto lg:w-[75rem]">
       <h1 className="mb-4 text-3xl font-bold">{title}</h1>
       <div className="w-full max-w-[55rem]">
         <div className="mb-4 text-gray-700">{author}</div>
@@ -37,17 +37,19 @@ export default function Article({
           </div>
         </div>
 
-        <div className="relative mb-4">
-          <AspectRatio ratio={16 / 9}>
-            <Image
-              src={imageL}
-              alt="Content Image"
-              fill
-              sizes="100vw"
-              className="rounded-lg object-contain"
-            />
-          </AspectRatio>
-        </div>
+        {imageL && (
+          <div className="relative mb-4">
+            <AspectRatio ratio={16 / 9}>
+              <Image
+                src={imageL}
+                alt="Content Image"
+                fill
+                sizes="100vw"
+                className="rounded-lg object-contain"
+              />
+            </AspectRatio>
+          </div>
+        )}
         <pre
           className={cn(
             "mb-4 whitespace-pre-wrap text-gray-600",
