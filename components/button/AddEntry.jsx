c685f2e0936ce2d6eade186cb306daa52ade3997
Paolo Lalap <paolomorales1502@gmail.com>
2024-05-27 09:46:21 +0800

    update ui

--- components/button/AddEntry.jsx
+++ components/button/AddEntry.jsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 
 export default function AddEntry({ children }) {
   return (
-    <Link href={"/add-content"} tabIndex={-1} className="mx-auto mt-5">
+    <Link href={"/add-content"} tabIndex={-1} className="mx-auto">
       <Button
         className="bg-[#8a1438] hover:bg-[#8a1438]/90"
         aria-label="add news button"
