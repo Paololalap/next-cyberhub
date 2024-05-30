fe142ea872cc073de0b796191a0eba9e451dd48d
Paolo Lalap <paolomorales1502@gmail.com>
2024-05-30 10:10:44 +0800

    remove fontsize

--- components/Items.jsx
+++ components/Items.jsx
@@ -2,13 +2,13 @@ import { ITEMS } from "@/constants/ITEMS";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Items({ fontSize }) {
+export default function Items() {
   return (
     <ul className="mb-[50px] space-y-2">
       {ITEMS.map((item, index) => (
         <li
           key={index}
-          className={`text-[#6a6a6a] text-[${fontSize}px] flex items-center gap-3`}
+          className={`text-[#6a6a6a] text-[15px] flex items-center gap-3`}
         >
           <Image
             src={item.icon}
--- components/UPSystems.jsx
+++ components/UPSystems.jsx
@@ -1,13 +1,13 @@
 import { UP_SYSTEMS } from "@/constants/UP_SYSTEMS";
 import Link from "next/link";
 
-export default function UPSystems({ fontSize }) {
+export default function UPSystems() {
   return (
     <ul className="space-y-2">
       {UP_SYSTEMS.map((system) => (
         <li
           key={system.name}
-          className={`text-[#8a1538] text-[${fontSize}px] `}
+          className={`text-[#8a1538] text-[15px] `}
         >
           <Link target="_blank" href={system.link}>
             {system.name}
